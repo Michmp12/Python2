36ce8db62427b3992f721a5e0e024982e0ae2137
MichMP <158522214+Michmp12@users.noreply.github.com>
2024-02-22 17:38:38 -0500

    Add files via upload

---
+++ Ejemplo2.py
@@ -0,0 +1,6 @@
+numeroInt = int(input('Ingrese un número'))
+
+if numeroInt >= 1 and numeroInt <= 10:
+    print('Adivinastes el rango de número')
+else:
+    print('Estas fuera del rango del número')
